5a0bb1415eba34fd2057a68fb393ff590b37a475
Ikraam <ibndaanis@gmail.com>
2024-08-07 23:26:41 +0100

    added notifications

--- lib/index.ts
+++ lib/index.ts
@@ -2,4 +2,5 @@ export { Box } from "./Box";
 export { Buttons } from "./Buttons";
 export { HeroSection } from "./HeroSection";
 export { Modal } from "./Modal";
+export { Notifications } from "./Notifications";
 export { TableOfContents } from "./TableOfContents";
