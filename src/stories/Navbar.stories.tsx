081590d8e9a1c9654542d984080d25beebf63769
Ikraam <ibndaanis@gmail.com>
2024-08-22 18:44:00 +0100

    added navbar

--- src/stories/Navbar.stories.tsx
+++ src/stories/Navbar.stories.tsx
@@ -1,123 +1,114 @@
 import type { Meta, StoryObj } from "@storybook/react";
-import "bulma/css/bulma.min.css";
-import { Navbar } from "../components/Navbar";
+import { EndButtonProps, Navbar } from "../components/Navbar";
 
-const meta = {
+const meta: Meta<typeof Navbar> = {
   title: "Components/Navbar",
   component: Navbar,
   parameters: {
     layout: "fullscreen",
   },
   tags: ["autodocs"],
-} satisfies Meta<typeof Navbar>;
-
-export default meta;
-type Story = StoryObj<typeof meta>;
-
-export const DefaultNavbar: Story = {
-  render: () => <Navbar />,
+  argTypes: {
+    brand: { control: "text" },
+    startItems: { control: "object" },
+    endButtons: { control: "object" },
+  },
 };
 
-const colors = [
-  "",
-  "primary",
-  "link",
-  "info",
-  "success",
-  "warning",
-  "danger",
-  "white",
-  "black",
-  "light",
-  "dark",
-] as const;
+export default meta;
+type Story = StoryObj<typeof Navbar>;
 
-type ColorType = (typeof colors)[number];
+const defaultBrand = (
+  <span className="navbar-item">
+    <img
+      src="/markdown-space-logo.svg"
+      alt="Bulma: a modern CSS framework based on Flexbox"
+      height="28"
+    />
+  </span>
+);
 
-const createNavbarStory = (color: ColorType): Story => ({
-  render: () => (
-    <nav className={`navbar ${color ? `is-${color}` : ""}`}>
-      <div className="navbar-brand">
-        <a className="navbar-item" href="https://bulma.io">
-          <img src="/markdown-space-logo.svg" alt="Logo" height="28" />
-        </a>
-        <div className="navbar-burger" aria-label="menu" aria-expanded="false">
-          <span aria-hidden="true"></span>
-          <span aria-hidden="true"></span>
-          <span aria-hidden="true"></span>
-        </div>
-      </div>
-      <div className="navbar-menu">
-        <div className="navbar-start">
-          <a className="navbar-item">Home</a>
-          <a className="navbar-item">Documentation</a>
-        </div>
-        <div className="navbar-end">
-          <div className="navbar-item">
-            <div className="buttons">
-              <a className="button is-primary">Sign up</a>
-              <a className="button is-light">Log in</a>
-            </div>
-          </div>
-        </div>
-      </div>
-    </nav>
-  ),
-});
+const defaultStartItems = [
+  { label: "Home" },
+  { label: "Documentation" },
+  {
+    label: "More",
+    items: [
+      { label: "About" },
+      { label: "Jobs", isSelected: true },
+      { label: "Contact" },
+      { label: "Report an issue" },
+    ],
+  },
+];
 
-export const DefaultColorNavbar = createNavbarStory("");
-export const PrimaryNavbar = createNavbarStory("primary");
-export const LinkNavbar = createNavbarStory("link");
-export const InfoNavbar = createNavbarStory("info");
-export const SuccessNavbar = createNavbarStory("success");
-export const WarningNavbar = createNavbarStory("warning");
-export const DangerNavbar = createNavbarStory("danger");
-export const WhiteNavbar = createNavbarStory("white");
-export const BlackNavbar = createNavbarStory("black");
-export const LightNavbar = createNavbarStory("light");
-export const DarkNavbar = createNavbarStory("dark");
+const defaultEndButtons: EndButtonProps[] = [
+  { label: "Sign up", color: "primary" },
+  { label: "Log in", color: "light" },
+];
 
-export const TransparentNavbar: Story = {
-  render: () => (
-    <nav className="navbar is-transparent">
-      <div className="navbar-brand">
-        <a className="navbar-item" href="https://bulma.io">
-          <img
-            src="/markdown-space-logo.svg"
-            alt="Logo"
-            width="112"
-            height="28"
-          />
-        </a>
-        <div className="navbar-burger" aria-label="menu" aria-expanded="false">
-          <span aria-hidden="true"></span>
-          <span aria-hidden="true"></span>
-          <span aria-hidden="true"></span>
-        </div>
-      </div>
-      <div className="navbar-menu">
-        <div className="navbar-start">
-          <a className="navbar-item">Home</a>
-          <div className="navbar-item has-dropdown is-hoverable">
-            <a className="navbar-link">Docs</a>
-            <div className="navbar-dropdown is-boxed">
-              <a className="navbar-item">Overview</a>
-              <a className="navbar-item">Elements</a>
-              <a className="navbar-item">Components</a>
-            </div>
-          </div>
-        </div>
-        <div className="navbar-end">
-          <div className="navbar-item">
-            <div className="buttons">
-              <a className="button is-primary">
-                <strong>Sign up</strong>
-              </a>
-              <a className="button is-light">Log in</a>
-            </div>
-          </div>
-        </div>
-      </div>
-    </nav>
-  ),
+export const Default: Story = {
+  args: {
+    brand: defaultBrand,
+    startItems: defaultStartItems,
+    endButtons: defaultEndButtons,
+  },
+};
+
+export const WithoutDropdown: Story = {
+  args: {
+    ...Default.args,
+    startItems: [
+      { label: "Home" },
+      { label: "Documentation" },
+      { label: "About" },
+      { label: "Contact" },
+    ],
+  },
+};
+
+export const WithoutEndButtons: Story = {
+  args: {
+    ...Default.args,
+    endButtons: [],
+  },
+};
+
+export const CustomBrand: Story = {
+  args: {
+    ...Default.args,
+    brand: (
+      <a className="navbar-item" href="#">
+        <strong>CUSTOM BRAND</strong>
+      </a>
+    ),
+  },
+};
+
+export const ManyItems: Story = {
+  args: {
+    ...Default.args,
+    startItems: [
+      { label: "Home" },
+      { label: "Products" },
+      { label: "Services" },
+      { label: "About" },
+      { label: "Contact" },
+      {
+        label: "More",
+        items: [
+          { label: "Team" },
+          { label: "Careers" },
+          { label: "Blog" },
+          { label: "FAQ" },
+        ],
+      },
+    ],
+  },
+};
+
+export const Playground: Story = {
+  args: {
+    ...Default.args,
+  },
 };
