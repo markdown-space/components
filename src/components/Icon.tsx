32be742ad4ad6ffca97410c20f68c144b98afecc
Ikraam <ibndaanis@gmail.com>
2024-09-03 20:30:11 +0100

    added more props to icon

--- src/components/Icon.tsx
+++ src/components/Icon.tsx
@@ -1,14 +1,41 @@
-import { ComponentProps, ReactNode } from "react";
+import { ComponentProps, ElementType, ReactNode } from "react";
+
+export type Color =
+  | "white"
+  | "black"
+  | "light"
+  | "dark"
+  | "primary"
+  | "link"
+  | "info"
+  | "success"
+  | "warning"
+  | "danger"
+  | "black-bis"
+  | "black-ter"
+  | "grey-darker"
+  | "grey-dark"
+  | "grey"
+  | "grey-light"
+  | "grey-lighter"
+  | "white-ter"
+  | "white-bis"
+  | "current"
+  | "inherit";
 
 export interface IconProps extends ComponentProps<"span"> {
   /** Font Awesome Icon class name */
   icon: string;
+  textColor?: Color;
+  size?: "small" | "medium" | "large";
 }
 
-export const Icon = ({ icon }: IconProps) => {
+export const Icon = ({ icon, textColor, size }: IconProps) => {
   const baseClass = "icon";
+  const colorClass = textColor ? `has-text-${textColor}` : "";
+  const sizeClass = size ? `is-${size}` : "";
 
-  const classes = [baseClass].filter(Boolean).join(" ");
+  const classes = [baseClass, colorClass, sizeClass].filter(Boolean).join(" ");
 
   return (
     <span className={classes}>
@@ -17,14 +44,34 @@ export const Icon = ({ icon }: IconProps) => {
   );
 };
 
-interface IconTextProps extends ComponentProps<"span"> {
-  /** All icon text must be wrapped around a span tag. */
+interface IconTextProps<T extends ElementType = "span"> {
+  /** The element type to render. Defaults to "span". */
+  as?: T;
+  /** The content of the IconText component. */
   children: ReactNode;
+  /** Additional CSS classes to apply to the component. */
+  className?: string;
+  textColor?: Color;
 }
 
-export const IconText = ({ className, children }: IconTextProps) => {
-  const baseClass = "icon-text";
-  const classes = [baseClass, className].filter(Boolean).join(" ");
+type IconTextComponentProps<T extends ElementType> = IconTextProps<T> &
+  Omit<ComponentProps<T>, keyof IconTextProps>;
 
-  return <span className={classes}>{children}</span>;
+export const IconText = <T extends ElementType = "span">({
+  as,
+  className,
+  children,
+  textColor,
+  ...props
+}: IconTextComponentProps<T>) => {
+  const Component = as || "span";
+  const baseClass = "icon-text";
+  const colorClass = textColor ? `has-text-${textColor}` : "";
+  const classes = [baseClass, colorClass, className].filter(Boolean).join(" ");
+
+  return (
+    <Component className={classes} {...props}>
+      {children}
+    </Component>
+  );
 };
