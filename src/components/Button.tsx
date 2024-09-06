2bf3f854d72ad607dff0a4ca12f95564a59dae55
Ikraam <ibndaanis@gmail.com>
2024-09-06 22:10:38 +0100

    button can now be a or input

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,7 +1,9 @@
-import { ButtonHTMLAttributes } from "react";
+import { ComponentPropsWithoutRef, forwardRef, ReactElement, Ref } from "react";
 import { Color, Size } from "../types/shared";
 
-export interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
+type ElementType = "button" | "a" | "input";
+
+type ButtonBaseProps = {
   color?: Color;
   size?: Size;
   isOutlined?: boolean;
@@ -16,61 +18,88 @@ export interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
   isSkeleton?: boolean;
   fullWidth?: boolean;
   theme?: "light" | "dark";
-}
-
-export const Button = ({
-  children,
-  color,
-  size,
-  isOutlined = false,
-  isInverted = false,
-  isRounded = false,
-  isLoading = false,
-  isActive = false,
-  isFocused = false,
-  isHovered = false,
-  fullWidth = false,
-  isSkeleton = false,
-  theme = undefined,
-  className,
-  ...props
-}: ButtonProps) => {
-  const baseClasses = "button";
-  const colorClasses = color ? `is-${color}` : "";
-  const sizeClasses = size ? `is-${size}` : "";
-  const outlinedClass = isOutlined ? "is-outlined" : "";
-  const invertedClass = isInverted ? "is-inverted" : "";
-  const roundedClass = isRounded ? "is-rounded" : "";
-  const loadingClass = isLoading ? "is-loading" : "";
-  const activeClass = isActive ? "is-active" : "";
-  const focusedClass = isFocused ? "is-focused" : "";
-  const hoveredClass = isHovered ? "is-hovered" : "";
-  const skeletonClass = isSkeleton ? "is-skeleton" : "";
-  const fullWidthClass = fullWidth ? "is-fullwidth" : "";
-  const themeClass = theme ? `is-${theme}` : "";
-
-  const buttonClasses = [
-    baseClasses,
-    colorClasses,
-    sizeClasses,
-    outlinedClass,
-    invertedClass,
-    roundedClass,
-    loadingClass,
-    activeClass,
-    focusedClass,
-    hoveredClass,
-    skeletonClass,
-    fullWidthClass,
-    themeClass,
-    className,
-  ]
-    .filter(Boolean)
-    .join(" ");
-
-  return (
-    <button className={buttonClasses} {...props}>
-      {children}
-    </button>
-  );
 };
+
+type ButtonTypeProps<T extends ElementType> = ButtonBaseProps &
+  (T extends "button"
+    ? { as?: T; type?: "submit" | "reset" | "button" }
+    : T extends "input"
+      ? { as: T; type?: "submit" | "reset" }
+      : { as: T }) &
+  Omit<ComponentPropsWithoutRef<T>, "as" | keyof ButtonBaseProps | "type">;
+
+export type ButtonProps<T extends ElementType = "button"> = ButtonTypeProps<T>;
+
+type ButtonRef<T extends ElementType> = T extends "button"
+  ? HTMLButtonElement
+  : T extends "a"
+    ? HTMLAnchorElement
+    : T extends "input"
+      ? HTMLInputElement
+      : never;
+
+export const Button = forwardRef(
+  <T extends ElementType = "button">(
+    {
+      children,
+      color,
+      size,
+      isOutlined = false,
+      isInverted = false,
+      isRounded = false,
+      isLoading = false,
+      isActive = false,
+      isFocused = false,
+      isHovered = false,
+      fullWidth = false,
+      isSkeleton = false,
+      theme = undefined,
+      className,
+      as,
+      ...rest
+    }: ButtonProps<T>,
+    ref: Ref<ButtonRef<T>>,
+  ) => {
+    const Component = as || "button";
+    const baseClasses = "button";
+    const colorClasses = color ? `is-${color}` : "";
+    const sizeClasses = size ? `is-${size}` : "";
+    const outlinedClass = isOutlined ? "is-outlined" : "";
+    const invertedClass = isInverted ? "is-inverted" : "";
+    const roundedClass = isRounded ? "is-rounded" : "";
+    const loadingClass = isLoading ? "is-loading" : "";
+    const activeClass = isActive ? "is-active" : "";
+    const focusedClass = isFocused ? "is-focused" : "";
+    const hoveredClass = isHovered ? "is-hovered" : "";
+    const skeletonClass = isSkeleton ? "is-skeleton" : "";
+    const fullWidthClass = fullWidth ? "is-fullwidth" : "";
+    const themeClass = theme ? `is-${theme}` : "";
+    const buttonClasses = [
+      baseClasses,
+      colorClasses,
+      sizeClasses,
+      outlinedClass,
+      invertedClass,
+      roundedClass,
+      loadingClass,
+      activeClass,
+      focusedClass,
+      hoveredClass,
+      skeletonClass,
+      fullWidthClass,
+      themeClass,
+      className,
+    ]
+      .filter(Boolean)
+      .join(" ");
+
+    return (
+      // eslint-disable-next-line @typescript-eslint/no-explicit-any
+      <Component className={buttonClasses} ref={ref as any} {...(rest as any)}>
+        {children}
+      </Component>
+    );
+  },
+) as <T extends ElementType = "button">(
+  props: ButtonProps<T> & { ref?: Ref<ButtonRef<T>> },
+) => ReactElement;
