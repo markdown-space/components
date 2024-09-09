002cf697d5339039dcb4b6a9ccd4669d7b043e29
Ikraam <ibndaanis@gmail.com>
2024-09-09 19:30:14 +0100

    added message component

--- src/components/Message.tsx
+++ src/components/Message.tsx
@@ -1,37 +1,36 @@
-export const Message = () => {
-  const modifiers = [
-    "",
-    "primary",
-    "link",
-    "info",
-    "success",
-    "warning",
-    "danger",
-  ];
+import { ComponentProps, ReactNode } from "react";
+
+export interface MessageProps extends ComponentProps<"article"> {
+  color?:
+    | "dark"
+    | "link"
+    | "primary"
+    | "info"
+    | "success"
+    | "warning"
+    | "danger";
+  header?: ReactNode;
+  size?: "small" | "medium" | "large";
+}
+
+export const Message = ({ color, header, size, children }: MessageProps) => {
+  const baseClasses = "message";
+  const colorClass = color ? `is-${color}` : "";
+  const sizeClass = size ? `is-${size}` : "";
+
+  const classes = [baseClasses, colorClass, sizeClass]
+    .filter(Boolean)
+    .join(" ");
 
   return (
-    <section className="section" id="message">
-      <div className="columns is-multiline">
-        {modifiers.map((m, index) => (
-          <div key={index} className="column is-half">
-            <article className={`message ${m ? `is-${m}` : ""}`}>
-              <div className="message-header">
-                <p>{m ? m.charAt(0).toUpperCase() + m.slice(1) : "Message"}</p>
-                <button className="delete"></button>
-              </div>
-              <div className="message-body">
-                In the tapestry of life, each thread weaves a unique story.{" "}
-                <strong>Our choices, big and small</strong>, color the fabric of
-                our journey. With every step, we paint our path forward, leaving
-                behind a trail of memories. It's in the quiet moments that we
-                find
-                <a>our true selves</a>, reflecting on the past and dreaming of{" "}
-                <em>future adventures</em>.
-              </div>
-            </article>
-          </div>
-        ))}
-      </div>
-    </section>
+    <article className={classes}>
+      {header && (
+        <div className="message-header">
+          <p>{header}</p>
+          <button className="delete" aria-label="delete"></button>
+        </div>
+      )}
+      <div className="message-body">{children}</div>
+    </article>
   );
 };
