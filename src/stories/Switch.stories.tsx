99d862083c65604c1be9ba410b34f514492fb34d
Ikraam <ibndaanis@gmail.com>
2024-10-02 22:20:00 +0100

    disabled styling, more stories

--- src/stories/Switch.stories.tsx
+++ src/stories/Switch.stories.tsx
@@ -1,5 +1,6 @@
 import type { Meta, StoryObj } from "@storybook/react";
 import { useState } from "react";
+import { FormField } from "../components/FormField";
 import { Switch } from "../components/Switch";
 
 export default {
@@ -9,25 +10,84 @@ export default {
     layout: "centered",
     docs: {
       description: {
-        component: "A simple Switch component.",
+        component: "A customizable Switch component with styled-components.",
       },
     },
   },
   tags: ["autodocs"],
-  argTypes: {},
+  argTypes: {
+    checked: {
+      control: "boolean",
+      description: "The checked state of the switch",
+    },
+    onCheckedChange: {
+      action: "checked changed",
+      description: "Callback function when the switch state changes",
+    },
+  },
 } satisfies Meta<typeof Switch>;
 
 type Story = StoryObj<typeof Switch>;
 
-export const Default: Story = {
-  render: () => {
-    const [checked, setChecked] = useState(false);
-
+const SwitchTemplate: Story = {
+  render: (args) => {
+    const [checked, setChecked] = useState(args.checked);
     return (
       <Switch
+        {...args}
         checked={checked}
-        onCheckedChange={(checked) => setChecked(checked)}
+        onCheckedChange={(newChecked) => {
+          setChecked(newChecked);
+          args.onCheckedChange(newChecked);
+        }}
       />
     );
+  },
+};
+
+export const Default: Story = {
+  ...SwitchTemplate,
+  args: {
+    checked: false,
+  },
+};
+
+export const Checked: Story = {
+  ...SwitchTemplate,
+  args: {
+    checked: true,
+  },
+};
+
+export const WithLabel: Story = {
+  ...SwitchTemplate,
+  render: (args) => {
+    const [checked, setChecked] = useState(args.checked);
+    return (
+      <FormField
+        label="Enable feature"
+        customInput={
+          <Switch
+            {...args}
+            checked={checked}
+            onCheckedChange={(newChecked) => {
+              setChecked(newChecked);
+              args.onCheckedChange(newChecked);
+            }}
+          />
+        }
+      ></FormField>
+    );
+  },
+  args: {
+    checked: false,
+  },
+};
+
+export const Disabled: Story = {
+  ...SwitchTemplate,
+  args: {
+    checked: false,
+    disabled: true,
   },
 };
