72b69b52014962d86f6aed4d92d1ffd10a791255
Ikraam <ibndaanis@gmail.com>
2024-10-04 20:35:21 +0100

    themecolor type

--- src/types/theme.d.ts
+++ src/types/theme.d.ts
@@ -1,6 +1,31 @@
+type ThemeColor =
+  | "default"
+  | "cerulean"
+  | "cosmo"
+  | "cyborg"
+  | "darkly"
+  | "flatly"
+  | "journal"
+  | "litera"
+  | "lumen"
+  | "lux"
+  | "materia"
+  | "minty"
+  | "pulse"
+  | "nuclear"
+  | "sandstone"
+  | "simplex"
+  | "slate"
+  | "solar"
+  | "spacelab"
+  | "superhero"
+  | "united"
+  | "yeti"
+  | "unicorn";
+
 type Theme = {
-  id: string;
+  id: ThemeColor;
   name: string;
   dataTheme: string;
   description?: string;
-}
+};
